1229e27a07807fbaa0ef1489fd4f7b21d99471e6
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-07-29 23:21:11 +0530

    token vesting -1

--- anchor/programs/counter/src/lib.rs
+++ anchor/programs/counter/src/lib.rs
@@ -1,24 +1,57 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+use anchor_spl::{token::TokenAccount, token_interface::TokenInterface};
 
 declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
 
 #[program]
 pub mod vesting {
-   pub fn create_vesting_account(ctx:Context<CreateVestingAccount>) -> Result<()> {
+   pub fn create_vesting_account(ctx:Context<CreateVestingAccount> , company_name:String) -> Result<()> {
       Ok(())
    }
 }
 
 #[derive(Accounts)]
-pub struct CreateVestingAccount<'info>{
-   #[account(mut)]
-   pub signer = Signer<'info>,
+#[instruction(company_name:String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-   #[account(
-      init,
-      spac ``
-   )]
+    #[account(
+        init,
+        space = 8 + INIT_SPACE,
+        payer = signer,
+        seeds = [company_name.as_ref()],
+         bump 
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    pub mint: InterfaceAccount<'info , Mint>,
+    
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        payer = signer,
+        seeds = [b"vesting_treasury" , company_name.as_bytes()],
+        bump,
+    )]
 
+    pub treasury_token_account: InterfaceAccount<'info , TokenAccount>,
+
+    pub system_program: Program<'info , System>,
+    pub token_program: Interface<'info , TokenInterface>,
+
+
+#[account]
+#[derive(InitSpace)]
+
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
 }
